b191ea2d7cac0c33f8e8002dcfdc203851767526
David Wong <david.c.wong@intel.com>
2022-10-26 19:02:05 +0000

    Use updated app_builder script.

--- scripts/app_mutator.py
+++ scripts/app_mutator.py
@@ -32,6 +32,31 @@ def instantiate_template(template_file, names, out_dir, outfile_name):
     return full_filename
 
 def generate_locus_file_and_scripts(dbfile, run_dir, compiler, src_file, codelet_name, locus_bin_run_dir, inc_flags):
+    iceorig_file, locus_outfile_suffix, build_cmd_sh_file, timing_script_file, timing_fn_name = generate_build_run_timing_scripts(run_dir, src_file, codelet_name, locus_bin_run_dir)
+
+    locus_file, preproc_folders, locus_run_cmd = generate_locus_command(dbfile, src_file, codelet_name, inc_flags, locus_outfile_suffix, timing_script_file, timing_fn_name)
+                    
+    # returning iceorig_file to be restored by build_cmd script after each Locus search step
+    # Also return locus_run_cmd to be used
+    return iceorig_file, locus_run_cmd, dbfile, locus_file, preproc_folders, build_cmd_sh_file
+
+def generate_locus_command(dbfile, src_file, inc_flags, locus_outfile_suffix, timing_script_file, timing_fn_name):
+    # Locus will invoke build_cmd script and that script will make use of full_iceorig_file to restore back to full_src_file
+    #engine='exhaustive'
+    engine='opentuner'
+    ntests=500
+    #ntests=50
+    locus_file='scop.locus'
+    preproc_folders = [part.strip() for part in inc_flags.split("-I") if part]
+    preproc_folders_str = " ".join(preproc_folders)
+    locus_run_cmd = [f'ice-locus-{engine}.py --database {dbfile} -f {src_file} '
+                    f'-t {locus_file} -o suffix --search --ntests {ntests} --tfunc {timing_script_file}:{timing_fn_name} '
+                    f'--preproc {preproc_folders_str} '
+                    f'--suffixinfo qaas --equery --no-applyDFAOpt --suffix {locus_outfile_suffix} --debug' ]
+                    
+    return locus_file,preproc_folders,locus_run_cmd
+
+def generate_build_run_timing_scripts(run_dir, src_file, codelet_name, locus_bin_run_dir):
     #src_file='bt.c_compute_rhs_line1892_loop.c'
     #src_file='bt.c_compute_rhs_line1907_loop.c'
     iceorig_file=src_file+'.iceorig'
@@ -54,7 +79,8 @@ def generate_locus_file_and_scripts(dbfile, run_dir, compiler, src_file, codelet
 		+ f" --user-cxx-flags \"$QAAS_user_cxx_flags\"" \
 		+ f" --user-fc-flags \"$QAAS_user_fc_flags\"" \
 		+ f" --user-link-flags \"$QAAS_user_link_flags\"" \
-		+ f" --user-target $QAAS_user_target"
+		+ f" --user-target $QAAS_user_target" \
+		+ f" --user-target-location $QAAS_user_target_location"
     run_cmd=f'python {script_dir}/app_runner.py' \
 		+ f" --binary-path $QAAS_binary_path" \
 		+ f" --run-dir $QAAS_run_dir" \
@@ -79,95 +105,7 @@ def generate_locus_file_and_scripts(dbfile, run_dir, compiler, src_file, codelet
     os.chmod(full_build_cmd_sh_file, stat.S_IRWXU)
     os.chmod(full_run_cmd_sh_file, stat.S_IRWXU)
     instantiate_template(timing_script_file+'.template', names, run_dir, timing_script_file)
-
-    # Locus will invoke build_cmd script and that script will make use of full_iceorig_file to restore back to full_src_file
-    #engine='exhaustive'
-    engine='opentuner'
-    ntests=500
-    #ntests=50
-    locus_file='scop.locus'
-    preproc_folders = [part.strip() for part in inc_flags.split("-I") if part]
-    preproc_folders_str = " ".join(preproc_folders)
-    locus_run_cmd = [f'ice-locus-{engine}.py --database {dbfile} -f {src_file} '
-                    f'-t {locus_file} -o suffix --search --ntests {ntests} --tfunc {timing_script_file}:{timing_fn_name} '
-                    f'--preproc {preproc_folders_str} '
-                    f'--suffixinfo {codelet_name}  --equery --no-applyDFAOpt --suffix {locus_outfile_suffix} --debug' ]
-                    
-    # returning iceorig_file to be restored by build_cmd script after each Locus search step
-    # Also return locus_run_cmd to be used
-    return iceorig_file, locus_run_cmd, dbfile, locus_file, preproc_folders
-
-# Execution scheme:
-# 
-# Given original file <file>
-# save <file> to <file>.orig to restore at the end
-#
-# Change, in place, file <file> original pragma to ICE pragma  (<file> now updated to contain ICE pragma)
-# Copy <file> to <file>.iceorig
-# Invoke LORE with <file> as input and suffix .opt so output <file>.opt
-# In build command (NOT IN PYTHON SCRIPT), 
-#   mv <file>.opt to <file>
-#   proceed to normal building procedure (so LORE output file will be used)
-#   cp <file>.iceorig to <file> to restore it for next steps of search
-# after all is done
-#   mv <file>.orig <file>
-def run_mutator(dbfile, compiler, full_src_file, num_cores):
-    run_dir = os.path.dirname(full_src_file)
-    src_file = os.path.basename(full_src_file)
-
-    #num_cores = multiprocessing.cpu_count()
-    old_layout = os.path.split(run_dir)[1] == 'extractedLoops'
-
-    iceorig_file, locus_run_cmd, dbfile = generate_locus_file_and_scripts(dbfile, run_dir, 
-        compiler, src_file, num_cores, old_layout)
-
-    #full_src_file=os.path.join(run_dir, src_file0)
-    restore_src_file=src_file + '.orig'
-    full_iceorig_file=os.path.join(run_dir, iceorig_file)
-
-
-    shutil.copy2(full_src_file, os.path.join(run_dir, restore_src_file)) # save restore file
-    input('test')
-    with open(full_src_file, 'r') as rd:
-        lines = rd.readlines()
-    # change #pragma scop to ICE pragma
-    visited_begin_pragma_scop = False
-    with open(full_src_file, 'w') as wr:
-        for line in lines:
-            if re.match('#pragma scop', line):
-                if old_layout:
-                    # old layout format:
-                    # #pragma scop
-                    #    for...
-                    # so replace pragma in place
-                    line = re.sub('#pragma scop', '#pragma @ICE loop=scop', line)
-                else:
-                    # new layout format:
-                    # #pragma scop
-                    # {
-                    #    for...
-                    # }
-                    # so insert pragma after bracket
-                    line = re.sub('#pragma scop', '', line)
-                    visited_begin_pragma_scop = True
-            if re.match('{', line) and old_layout is False and visited_begin_pragma_scop is True:
-                # write the original bracket line
-                wr.write(line)
-                # insert the ICE pragma after this bracket
-                line = '#pragma @ICE loop=scop\n'
-                visited_begin_pragma_scop = False
-            line = re.sub('#pragma endscop', '', line)
-            wr.write(line)
-    # full_src_file now have ICE pragma instead
-    # save to iceorig file.  Note this file will be used by build_cmd scrpt to restore it after each Locus search step
-    shutil.copy2(full_src_file, full_iceorig_file) 
-
-    my_env=os.environ.copy()
-    my_env['LORE_CC_VERSION'] = subprocess.run([compiler, '--version'], capture_output=True, text=True).stdout.split('\n')[0]
-    my_env['LORE_CC'] = compiler
-    my_env['LORE_CXX'] = compiler
-    #subprocess.run(locus_run_cmd, shell=True, cwd=run_dir, env=my_env)
-    shutil.copy2(os.path.join(run_dir, restore_src_file), full_src_file) 
+    return iceorig_file,locus_outfile_suffix,build_cmd_sh_file,timing_script_file,timing_fn_name
 
 # Execution scheme:
 # 
@@ -186,7 +124,7 @@ def run_mutator(dbfile, compiler, full_src_file, num_cores):
 #   mv <file>.orig <file>
 def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC, 
          user_c_flags, user_cxx_flags, user_fc_flags, user_link_flags, user_target,
-         binary_path, run_dir, data_path, run_cmd, env_var_map):
+         binary_path, run_dir, data_path, run_cmd, env_var_map, target_location):
     my_env = os.environ.copy()
 
     locus_timestamp = int(round(datetime.datetime.now().timestamp()))
@@ -212,9 +150,10 @@ def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC,
     
     build_dir, output_dir, output_name, env = setup_build(locus_src_dir, compiler_dir, locus_bin, orig_user_CC, user_CC, 
                                                           user_c_flags, user_cxx_flags, user_fc_flags, user_link_flags)
+    full_iceorig_file, locus_outfile_suffix, build_cmd_sh_file, timing_script_file, timing_fn_name = generate_build_run_timing_scripts(run_dir, full_src_file, 'conv', locus_bin_run_dir)
     #build_binary(user_target, build_dir, env, output_dir, output_name)
     # See script generation function for the user of these variables
-    make_sh_cmd="./buildcmd.sh"
+    make_sh_cmd=f'./{build_cmd_sh_file}'
     env["QAAS_locus_src_dir"]=f"{locus_src_dir}"
     env["QAAS_binary_path"]=f"{locus_bin}"
     env["QAAS_compiler_dir"]=f"{compiler_dir}"
@@ -225,6 +164,7 @@ def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC,
     env["QAAS_user_fc_flags"]=f"{user_fc_flags}"
     env["QAAS_user_link_flags"]=f"{user_link_flags}"
     env["QAAS_user_target"]=f"{user_target}"
+    env["QAAS_user_target_location"]=f"{target_location}"
 
     # below command try the make script
     subprocess.run(make_sh_cmd, shell=True, env=env, cwd=run_dir)
@@ -248,8 +188,8 @@ def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC,
     # Will be created by Locus
     dbfile=os.path.join(run_dir,'lore-locus.db')
 
-    full_iceorig_file, locus_run_cmd, dbfile, locus_file, header_folders = generate_locus_file_and_scripts(dbfile, run_dir, 
-        user_CC, full_src_file, 'conv', locus_bin_run_dir, inc_flags)
+    locus_file, header_folders, locus_run_cmd = generate_locus_command(dbfile, full_src_file, inc_flags, locus_outfile_suffix, timing_script_file, timing_fn_name)
+
     print(full_iceorig_file)
     print(locus_run_cmd)
     print(dbfile)
@@ -277,8 +217,6 @@ def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC,
     # save to iceorig file.  Note this file will be used by build_cmd scrpt to restore it after each Locus search step
     shutil.copy2(full_src_file, full_iceorig_file) 
 
-
-
     prepare_run(locus_bin, locus_bin_run_dir, data_path)
 
     run_sh_cmd="./runcmd.sh"
@@ -302,7 +240,7 @@ def exec(src_dir, compiler_dir, relative_binary_path, orig_user_CC, user_CC,
     if extract_best_variant(dbfile, locus_file, locus_result_dir, header_folders):
         # Best variant regenerated in place, so rebuild will produce opt executable
         os.remove(locus_bin)
-        build_binary(user_target, build_dir, env, output_dir, output_name)
+        build_binary(user_target, build_dir, target_location, env, output_dir, output_name)
         # Copy the binary to result directory 
         os.makedirs(os.path.dirname(relative_binary_path), exist_ok=True)
         shutil.copy2(locus_bin, relative_binary_path)
@@ -357,7 +295,7 @@ def main():
     env_var_map = dict([(v.split("=",1)) for v in args.var]) 
     exec(args.src_dir, args.compiler_dir, args.relative_binary_path, args.orig_user_CC, args.target_CC,
          args.user_c_flags, args.user_cxx_flags, args.user_fc_flags, args.user_link_flags, args.user_target,
-         args.binary_path, args.run_dir, args.data_path, args.run_cmd, env_var_map)
+         args.binary_path, args.run_dir, args.data_path, args.run_cmd, env_var_map, args.user_target_location)
 
 if __name__ == "__main__": 
     main()
