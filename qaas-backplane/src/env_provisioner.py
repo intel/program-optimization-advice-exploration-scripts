3158d8ab1cb1a50d38a58265f6221663dcfa4245
Abdelhafid, Mazouz <abdelhafid.mazouz@intel.com>
2022-11-09 14:38:57 -0600

    [18] Update Provisioner

--- qaas-backplane/src/env_provisioner.py
+++ qaas-backplane/src/env_provisioner.py
@@ -1,52 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###############################################################################
+# TO BE FIXED
+# Copyright (C) 2022  Intel Corporation  All rights reserved
+###############################################################################
+# HISTORY
+# Created June 2022
+# Updated October 2022
 # Contributors: Hafid/David
+
 import os
-import shutil
-import tarfile
-import util
-from logger import log, QaasComponents
+import logging
+from utils.runcmd import QAASRunCMD
 
-script_dir=os.path.dirname(os.path.realpath(__file__))
-MOCKUP_DIR=os.path.join(script_dir, "..", "mockups")
-MOCKUP_SRC_TARBALL=os.path.join(MOCKUP_DIR, 'env_provisioner', 'src_tarball', 'conv-codelets.tar.gz')
-MOCKUP_DATA_TARBALL=os.path.join(MOCKUP_DIR, 'env_provisioner', 'data_tarball', 'data.tar.gz')
+# define QAAS GIT-related constants
+GIT_USER = "USER"
+GIT_TOKEN = "TOKEN"
+GIT_BRANCH = "BRANCH"
+GIT_SRC_URL = "SRC_URL"
+GIT_DATA_URL = "DATA_URL"
 
-def setup_environ(service_dir, src_url, data_url, docker_file):
-    download_dir=os.path.join(service_dir, 'download')
-    os.makedirs(download_dir)
-    log(QaasComponents.ENV_PROVISIONER, "Setting up environment...")
-    src_dir=os.path.join(service_dir, 'code', 'src')
-    #os.makedirs(src_dir)
-    log(QaasComponents.ENV_PROVISIONER, f'Downloading source code {src_url} to {src_dir}')
+# define directory structure constants
+WORKDIR_ROOT_INDEX  = 0
+BUILDDIR_INDEX      = 1
+RUNDIR_INDEX        = 2
+BASEDIR_INDEX       = 3
+OVDIR_INDEX         = 4
+LOCUSDIR_INDEX      = 5
+QAAS_RUN_TYPES = ["base_runs", "oneview_runs", "locus_runs"]
 
-    src_tarball_name=os.path.basename(src_url)
-    src_tarball_file=os.path.join(download_dir, src_tarball_name)
-    #response = requests.get(src_url)
-    #open(src_tarball_file, 'wb').write(response.content)
-    log(QaasComponents.ENV_PROVISIONER, 'Downloading src tarball', mockup=True)
-    shutil.copyfile(MOCKUP_SRC_TARBALL, src_tarball_file)
+class QAASEnvProvisioner:
+    """Object to manage environment setup."""
+    def __init__(self, service_dir, account, app_name, git_params, machine, container):
+        logging.debug("QAASEnvProvisioner Constructor")
+        # save mete information
+        self.service_dir = service_dir
+        self.account = account
+        self.app_name = app_name
+        # setup working directories
+        self.work_dirs = []
+        self.work_dirs.append(os.path.join(self.service_dir, self.account, self.app_name))
+        self.work_dirs.append(os.path.join(self.work_dirs[WORKDIR_ROOT_INDEX], "build"))
+        self.work_dirs.append(os.path.join(self.work_dirs[WORKDIR_ROOT_INDEX], "run"))
+        for workdir in QAAS_RUN_TYPES:
+            self.work_dirs.append(os.path.join(self.work_dirs[RUNDIR_INDEX], workdir))
+        # save git configuration
+        self.git_user = git_params[GIT_USER]
+        self.git_token = git_params[GIT_TOKEN]
+        self.git_branch = git_params[GIT_BRANCH]
+        self.git_src_url = git_params[GIT_SRC_URL]
+        self.git_data_url = git_params[GIT_DATA_URL]
+        # save target machine access parameters
+        self.machine = machine
+        self.image_name = container["QAAS_CONTAINER_IMAGE"] + ":" + container["QAAS_CONTAINER_TAG"]
+        self.image_uid = container["QAAS_CONTAINER_UID"]
 
-    #data_dir=os.path.join(service_dir, 'data')
-    #os.makedirs(data_dir)
-    log(QaasComponents.ENV_PROVISIONER, 'Downloading data tarball', mockup=True)
-    data_tarball_name=os.path.basename(data_url)
-    data_tarball_file=os.path.join(download_dir, data_tarball_name)
-    shutil.copyfile(MOCKUP_DATA_TARBALL, data_tarball_file)
+    def get_workdir(self, target):
+        """Get a given dir."""
+        if target == "root":
+            return self.work_dirs[WORKDIR_ROOT_INDEX]
+        elif target == "build":
+            return self.work_dirs[BUILDDIR_INDEX]
+        elif target == "run":
+            return self.work_dirs[RUNDIR_INDEX]
+        elif target == "base_runs":
+            return self.work_dirs[BASEDIR_INDEX]
+        elif target == "oneview_runs":
+            return self.work_dirs[OVDIR_INDEX]
+        elif target == "locus_runs":
+            return self.work_dirs[LOCUSDIR_INDEX]
 
+    def create_work_dirs(self):
+        """Create working directories."""
+        logging.info("Create Working Directories on %s", self.machine)
+        # craete dirs
+        cmds = "'" + "mkdir -p " + self.work_dirs[0]
+        for index in range(1, len(self.work_dirs), 1): 
+            cmds = cmds + " && " + "mkdir -p " + self.work_dirs[index]
+        cmds = cmds + "'"
+        rc, cmdout = QAASRunCMD(self.machine).run_remote_cmd(cmds)
+        if rc != 0:
+            return rc
+        # update ownership rights for container runs 
+        rc = self.update_workdir_owner()
+        return rc
 
-    full_src_dir=extract_tarball(service_dir, download_dir, src_tarball_name, src_tarball_file, 'code')
-    full_data_dir=extract_tarball(service_dir, download_dir, data_tarball_name, data_tarball_file, 'data')
-    ov_run_dir = util.make_dir(os.path.join(service_dir, 'ov_runs'))
-    locus_run_dir = util.make_dir(os.path.join(service_dir, 'locus_runs'))
-    generated_image = 'MOCKUP_IMG'
-    log(QaasComponents.ENV_PROVISIONER, f'Building image using {docker_file} to generate image {generated_image}', mockup=True)
-    return full_src_dir, full_data_dir, ov_run_dir, locus_run_dir, generated_image
+    def update_workdir_owner(self):
+        """Update working directories ownership rights to build & run in a container."""
+        cmdline = "podman unshare chown -R :" + self.image_uid + " " + self.get_workdir("root")
+        rc, cmdout = QAASRunCMD(self.machine).run_remote_cmd(cmdline)
+        return rc
 
-def extract_tarball(service_dir, download_dir, src_tarball_name, src_tarball_file, dest_dir):
-    with tarfile.open(src_tarball_file) as zip_ref:
-        zip_ref.extractall(download_dir)
-    extracted_dir=src_tarball_name.split('.')[0]
-    log(QaasComponents.ENV_PROVISIONER, f'Extracted {src_tarball_name} to {extracted_dir}')
-    shutil.move(os.path.join(download_dir, extracted_dir), service_dir)
-    full_dest_dir = os.path.join(service_dir, dest_dir)
-    os.rename(os.path.join(service_dir, extracted_dir), full_dest_dir)
-    return full_dest_dir
+    def clone_source_repo(self):
+        """Clone the application's GIT repo."""
+        logging.info("Cloning application GIT repo on %s", self.machine)
+        target_branch = self.git_branch
+        if self.git_branch == None:
+            target_branch = "master"
+        cmdline = "'cd " + self.get_workdir("build") + \
+                  " && if [[ ! -d " + self.app_name + " ]]; then" + \
+                  " git clone -b " + target_branch + \
+                  " " + self.git_src_url + " " + self.app_name + \
+                  " && rm -rf " + self.app_name + "/.git; fi'"
+        rc, cmdout = QAASRunCMD(self.machine).run_remote_cmd(cmdline)
+        return rc
