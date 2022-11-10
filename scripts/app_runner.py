df57e50383e0336a2ef0c8791b336d32059075d5
David Wong <david.c.wong@intel.com>
2022-11-10 00:12:21 -0600

    Updated runners to take care of env and mpi

--- scripts/app_runner.py
+++ scripts/app_runner.py
@@ -4,46 +4,42 @@ import shutil
 import os
 from util import parse_env_map 
 from fdo_lib import LProfProfiler
+from base_runner import BaseRunner
 
-def prepare(binary_path, run_dir, data_path):
-    os.makedirs(run_dir, exist_ok=True)
-    try:
-        shutil.copy(binary_path, run_dir)
-    except:
-        pass
-    try:
-        if os.path.isfile(data_path):
-            shutil.copy(data_path, run_dir)
-        else:
-            assert os.path.isdir(data_path)
-            for file in os.listdir(data_path): 
-                shutil.copy(os.path.join(data_path, file), run_dir)
-        
-    except:
-        pass
+class AppRunner(BaseRunner):
+    def __init__(self, run_dir):
+        self.run_dir = run_dir
+
+    # def exec(self, env, binary_path, data_path, run_cmd, mode,
+    #          mpi_run_command, mpi_num_processes, omp_num_threads, 
+    #          mpi_envs, omp_envs):
+    #     if mode == 'prepare' or mode == 'both':
+    #         self.prepare(binary_path, self.run_dir, data_path)
+    #     if mode == 'run' or mode == 'both':
+    #         self.run(binary_path, self.run_dir, run_cmd, 
+    #                  mpi_run_command, mpi_num_processes, omp_num_threads, mpi_envs, omp_envs, env)
     
-def run(binary_path, run_dir, run_cmd, env_var_map, my_env = os.environ.copy()):
-    #my_env = os.environ.copy()
 
-    my_env.update(env_var_map)
-    #env = load_compiler_env(compiler_dir)
-    #my_env.update(env)
-    binary_name = os.path.basename(binary_path)
-    #true_run_cmd='ls; echo $OMP_NUM_THREADS'
-    print(f"run_dir is: {run_dir}")
-    # try LProf
-    #shutil.copy2(MAQAO_BIN, run_dir) 
-    LProfProfiler().run_lprof_loop_profile(run_dir, my_env, run_cmd, binary_name)
+    def true_run(self, binary_path, run_dir, run_cmd, run_env, mpi_command):
+        binary_name = os.path.basename(binary_path)
+        #true_run_cmd='ls; echo $OMP_NUM_THREADS'
+        true_run_cmd=f'{mpi_command} {run_cmd}' if mpi_command else run_cmd
+        print(f"run_dir is: {run_dir}")
+        # try LProf
+        #shutil.copy2(MAQAO_BIN, run_dir) 
+        LProfProfiler().run_lprof_loop_profile(run_dir, run_env, true_run_cmd, binary_name)
 
 # copy executable binary to current directory,
 # copy data file to current directory,
 # set up env map
 # run command replacing <binary> by binary to executable binary
-def exec(binary_path, run_dir, data_path, run_cmd, env_var_map, mode):
-    if mode == 'prepare' or mode == 'both':
-        prepare(binary_path, run_dir, data_path)
-    if mode == 'run' or mode == 'both':
-        run(binary_path, run_dir, run_cmd, env_var_map)
+def exec(env, binary_path, run_dir, data_path, run_cmd, mode,
+         mpi_run_command=None, mpi_num_processes=1, omp_num_threads=1, 
+         mpi_envs={"I_MPI_PIN_PROCESSOR_LIST":"all:map=spread"}, omp_envs={}):
+    app_runner = AppRunner(run_dir)
+    app_runner.exec (env, binary_path, data_path, run_cmd, mode, 
+                     mpi_run_command, mpi_num_processes, omp_num_threads, 
+                     mpi_envs, omp_envs)
    
 def build_argparser(parser, include_mode=True):
     parser.add_argument('--binary-path', help='Path to executable binary', required=True)
@@ -60,7 +56,9 @@ def main():
     build_argparser(parser)
     args = parser.parse_args()
     env_var_map = parse_env_map(args)
-    exec(args.binary_path, args.run_dir, args.data_path, args.run_cmd, env_var_map, args.mode)
+    my_env = os.environ.copy()
+    my_env.update(env_var_map)
+    exec(my_env, args.binary_path, args.run_dir, args.data_path, args.run_cmd, args.mode)
 
 if __name__ == "__main__": 
     main()
