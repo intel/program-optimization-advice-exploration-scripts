6a6183009c2d4496b6baa824d23461df44e53add
yue.jiao <yuejiao@fxilab165.an.intel.com>
2022-11-23 13:05:17 -0600

    cleanup: updated install and run script to have general path

--- qaas-web/backend/install_pip.sh
+++ qaas-web/backend/install_pip.sh
@@ -2,7 +2,7 @@
 
 #FLASK_DIR=/nfs/site/proj/alac/members/yue/web-service
 #TODO: Ability to use non-script directory
-FLASK_DIR=/nfs/site/proj/alac/members/yue/qaas/qaas/qaas-web/backend
+FLASK_DIR=$(pwd)
 cd $FLASK_DIR
 python3 -m venv ./venv
 source ./venv/bin/activate
