1660ac31e0602a250547a249b6bd7c165871a94c
yue.jiao <yuejiao@fxilab165.an.intel.com>
2022-12-01 12:22:57 -0600

    cleanup: get rid of manifest table and copytree and rundir

--- qaas-web/backend/ovdb.py
+++ qaas-web/backend/ovdb.py
@@ -83,9 +83,6 @@ def copyDir(from_path, to_path):
         shutil.rmtree(to_path)
     shutil.copytree(from_path, to_path)
 
-
-
-
 #create manifest table
 def modify_path(path):
     sub_path =  str(pathlib.Path(*Path(path).parts))
@@ -97,15 +94,40 @@ def modify_path(path):
 
     # cleaned_path = sub_path
     return cleaned_path
+def generate_manifest_csv(exp_dir):
+    # TODO: fix binary name hardcoded
+    content=f"""type;run;usage;path;
+meta;;report_type;monorun;
+meta;0;run_name;run_0;
+virtual;0;executable;{exp_dir}/app/oneview_runs/orig/exec;
+file;0;expert_run;{exp_dir}/shared/run_0/expert_run.csv;
+file;0;expert_loops;{exp_dir}/shared/run_0/expert_loops.csv;
+file;0;config;{exp_dir}/shared/run_0/config.lua;
+file;0;localvars;{exp_dir}/shared/run_0/local_vars.csv;
+dir;0;lprof_dir;{exp_dir}/shared/lprof_npsu_run_0;
+dir;0;cqa_dir;{exp_dir}/static_data/cqa;
+dir;0;sources_dir;{exp_dir}/static_data/sources;
+dir;0;asm_dir;{exp_dir}/static_data/asm;
+dir;0;groups_dir;{exp_dir}/static_data/groups;
+dir;0;hierarchy_dir;{exp_dir}/static_data/hierarchy;
+file;0;global_metrics;{exp_dir}/shared/run_0/global_metrics.lua;
+file;0;categorization;{exp_dir}/shared/run_0/lprof_categorization.csv;
+dir;0;callchains_dir;{exp_dir}/shared/lprof_npsu_run_0/callchains;
+file;0;log;{exp_dir}/logs/log.txt;
+dir;0;logs_subdir;{exp_dir}/logs/run_0;
+dir;0;env_dir;{exp_dir}/shared/run_0/;"""
 
+    print("manifest file generated at ",os.path.join(exp_dir, 'manifest.csv'))
+    with open(os.path.join(exp_dir,'manifest.csv'), 'w') as f: f.write(content)
+    return os.path.join(exp_dir, 'manifest.csv')
 def main():
     #where experimental run is default ./expR1
-    exp_dir = sys.argv[1] if len(sys.argv) > 1 else 'expR1'
-    run_dir = sys.argv[2]
+    if len(sys.argv) != 2:
+        print(f"error, wrong number of arguments, should be 1, arguments are {sys.argv}")
+        return
+    run_ovdb(sys.argv[1])
 
-    run_ovdb(exp_dir, run_dir)
-
-def run_ovdb(exp_dir, run_dir):
+def run_ovdb(exp_dir):
     import configparser
     config_path = f"{os.path.dirname(os.path.realpath(__file__))}/../config/qaas-web.conf"
     print(config_path)
@@ -120,16 +142,20 @@ def run_ovdb(exp_dir, run_dir):
 
     print("this experiment is run in ",exp_dir)
 
-    localvar_path = f"{os.path.join(run_dir, exp_dir)}/static_data/local_vars.csv"
+    #permanent folder where files that are not in db are copied create if not exist
+
+    localvar_path = f"{exp_dir}/static_data/local_vars.csv"
     localvar_df = pandas.read_csv(localvar_path, sep=';')
     timestamp =  pandas.to_datetime(localvar_df.get('timestamp').iloc[0])
-    print("timestamp extraced from experimental run local var csv",timestamp)
-    manifest_path = f"{run_dir}/manifest.csv"
+    print("timestamp from path time",timestamp)
 
-    #where files that are not in db are copied create if not exist
     target_path = os.path.join(config['web']['PERM_DATA_FOLDER'],timestamp.strftime('%m_%d_%Y_%H_%M_%S'))
     if not os.path.exists(target_path):
         os.makedirs(target_path)
+
+    manifest_path = os.path.join(exp_dir, 'manifest.csv')
+    if not os.path.exists(manifest_path):
+        generate_manifest_csv(exp_dir)
 
     first_line = True
     with open(manifest_path,newline='') as file:
@@ -140,13 +166,12 @@ def run_ovdb(exp_dir, run_dir):
                 continue
 
             path = line.rstrip().split(";")[3]
-            full_path = path if os.path.isabs(path) else os.path.join(manifest_folder, path)
+            full_path = path if os.path.isabs(path) else exp_dir
             path_type = line.rstrip().split(";")[0].strip()
             #copy binary that is not inside exp
-            sub_path =  str(pathlib.Path(*Path(path).parts))
-            if len(Path(path).parts) > 1:
-                sub_path = str(pathlib.Path(*Path(path).parts[1:]))
-            new_path = target_path +"/"+ sub_path
+            sub_path =  full_path[len(exp_dir)+1:] if os.path.isabs(path) else ''
+            new_path = os.path.join(target_path ,sub_path)
+            print("created path",new_path)
             os.makedirs(os.path.dirname(new_path), exist_ok=True)
             
 
@@ -162,51 +187,18 @@ def run_ovdb(exp_dir, run_dir):
             elif path_type == "file": 
                 create_table_from_file(full_path, new_path, timestamp, engine)
 
-    manifest_df = pandas.read_csv(manifest_path, sep=';')
-    manifest_df["timestamp"] = timestamp
-    manifest_df['path'] = manifest_df['path'].apply(modify_path)
-    manifest_df.to_sql(name= "manifest", con=engine, if_exists='append',index=False)
-    localvar_df.to_sql(name= "local_vars", con=engine, if_exists='append',index=False)
+    
 
-def populate_database(exp_dir, run_dir):
-    generate_manifest_csv(exp_dir, run_dir)
-    print(f"Manifest file saved under: {run_dir}")
-    #ovdb_command=f'python3 {script_dir}/ovdb.py {exp_dir} {run_dir} '
-
-    #subprocess.run(ovdb_command, shell=True, cwd=run_dir)
-    run_ovdb(exp_dir, run_dir)
-
-    localvar_path = f"{run_dir}/{exp_dir}/static_data/local_vars.csv"
+def populate_database(exp_dir):
+    generate_manifest_csv(exp_dir)
+    print(f"populate_databse manifest file saved under: {exp_dir}")
+    run_ovdb(exp_dir)
+    localvar_path = f"{exp_dir}/static_data/local_vars.csv"
     localvar_df = pandas.read_csv(localvar_path, sep=';')
     query_time =  pandas.to_datetime(localvar_df.get('timestamp').iloc[0])
-    print("flask query time extraced from local var", query_time)
+
     return query_time
 
-def generate_manifest_csv(exp_dir, run_dir):
-    # TODO: fix binary name hardcoded
-    content=f"""type;run;usage;path;
-meta;;report_type;monorun;
-meta;0;run_name;run_0;
-virtual;0;executable;./{exp_dir}/app/oneview_runs/orig/exec;
-file;0;expert_run;./{exp_dir}/shared/run_0/expert_run.csv;
-file;0;expert_loops;./{exp_dir}/shared/run_0/expert_loops.csv;
-file;0;config;./{exp_dir}/shared/run_0/config.lua;
-file;0;localvars;./{exp_dir}/shared/run_0/local_vars.csv;
-dir;0;lprof_dir;./{exp_dir}/shared/lprof_npsu_run_0;
-dir;0;cqa_dir;./{exp_dir}/static_data/cqa;
-dir;0;sources_dir;./{exp_dir}/static_data/sources;
-dir;0;asm_dir;./{exp_dir}/static_data/asm;
-dir;0;groups_dir;./{exp_dir}/static_data/groups;
-dir;0;hierarchy_dir;./{exp_dir}/static_data/hierarchy;
-file;0;global_metrics;./{exp_dir}/shared/run_0/global_metrics.lua;
-file;0;categorization;./{exp_dir}/shared/run_0/lprof_categorization.csv;
-dir;0;callchains_dir;./{exp_dir}/shared/lprof_npsu_run_0/callchains;
-file;0;log;./{exp_dir}/logs/log.txt;
-dir;0;logs_subdir;./{exp_dir}/logs/run_0;
-dir;0;env_dir;./{exp_dir}/shared/run_0/;"""
-
-    print("manifest file generated at ",os.path.join(run_dir, 'manifest.csv'))
-    with open(os.path.join(run_dir,'manifest.csv'), 'w') as f: f.write(content)
 
 if __name__ == '__main__':
    main()
