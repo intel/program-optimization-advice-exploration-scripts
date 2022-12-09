97eaca54477eae154119194cfb8bd33dab5113d4
David Wong <david.c.wong@intel.com>
2022-12-08 21:45:31 -0600

    User full numactl path.

--- qaas-service/base_runner.py
+++ qaas-service/base_runner.py
@@ -57,10 +57,10 @@ class BaseRunner(ABC):
                      mpi_run_command, mpi_num_processes, omp_num_threads, mpi_envs, omp_envs, env)
 
     def pin_seq_run_cmd(self, run_cmd):
-        rc, cmdout = QAASRunCMD(0).run_local_cmd("numactl -H | awk '/cpus/ && $2>=max {max=$2}; END{print max}'")
+        rc, cmdout = QAASRunCMD(0).run_local_cmd("/usr/bin/numactl -H | awk '/cpus/ && $2>=max {max=$2}; END{print max}'")
         last_node = int(cmdout.decode("utf-8"))
-        rc, cmdout = QAASRunCMD(0).run_local_cmd(f"numactl -H | grep 'node {last_node}' | grep  'cpus' |cut -d: -f2|xargs -n1|sort -r -n|head -1")
+        rc, cmdout = QAASRunCMD(0).run_local_cmd(f"/usr/bin/numactl -H | grep 'node {last_node}' | grep  'cpus' |cut -d: -f2|xargs -n1|sort -r -n|head -1")
         last_core = int(cmdout.decode("utf-8"))
 
-        seq_run_cmd = f'numactl -m {last_node} -C {last_core} {run_cmd}'
+        seq_run_cmd = f'/usr/bin/numactl -m {last_node} -C {last_core} {run_cmd}'
         return seq_run_cmd
