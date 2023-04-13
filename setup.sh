33c5e611650c88c6b90339508e924e705289c600
David Wong <david.c.wong@intel.com>
2023-04-13 19:41:27 +0000

    Added pin tool building automation

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+PIN_TAR_BALL=pin-3.27-98718-gbeaa5d51e-gcc-linux.tar.gz
+PIN_TOOL_NAME=HuskyFuncTrace
 # Build source code extractor
 make clean; make
 
 # Build pin-based data extractor
-wget https://software.intel.com/sites/landingpage/pintool/downloads/pin-3.27-98718-gbeaa5d51e-gcc-linux.tar.gz
 cd ..
-tar xvf ../pin-3.27-98718-gbeaa5d51e-gcc-linux.tar.gz
+wget https://software.intel.com/sites/landingpage/pintool/downloads/${PIN_TAR_BALL}
+tar xvf ${PIN_TAR_BALL}
+rm ${PIN_TAR_BALL}
 ln -s pin-3.27-98718-gbeaa5d51e-gcc-linux pin
+
+export PIN_ROOT=$(readlink -f pin)
+
+pushd ${PIN_ROOT}/source/tools
+ln -s ../../../codelet-extractor/src/pin-trace-memory-accesses ${PIN_TOOL_NAME}
+cd ..
+# Add our pin tool to build list
+sed -i "s/ALL_TEST_DIRS := /ALL_TEST_DIRS := "${PIN_TOOL_NAME}"/g" makefile
+cd ${PIN_TOOL_NAME}
+mkdir obj-intel64
+make obj-intel64/${PIN_TOOL_NAME}.so
+echo "Done building pin tool : ${PIN_TOOL_NAME}"
+popd
