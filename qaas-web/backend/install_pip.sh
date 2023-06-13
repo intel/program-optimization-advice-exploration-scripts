f30e432bf4b06de631af06d439e450cada09facf
yuejiaointel <yue.jiao@intel.com>
2023-06-13 02:34:30 +0200

    got rid of venv and works

--- qaas-web/backend/install_pip.sh
+++ qaas-web/backend/install_pip.sh
@@ -2,6 +2,6 @@
 
 FLASK_DIR=$(pwd)
 cd $FLASK_DIR
-python3 -m venv ./venv
-source ./venv/bin/activate
-pip3 install  -r requirements.txt
+# python3 -m venv ./venv
+# source ./venv/bin/activate
+sudo pip3 install  -r requirements.txt
