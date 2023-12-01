3772226f23746d8888ffe1f814d663a5d03e1aee
David Wong <david.c.wong@intel.com>
2023-11-30 18:04:06 -0600

    Updated to rebuild web folder on demand.

--- container/run-container.sh
+++ container/run-container.sh
@@ -52,6 +52,7 @@ docker volume create mysql_data
 docker volume create letsencrypt_data
 docker volume create mods_enabled_data
 docker volume create htpasswd_data
+docker volume create www_html_data
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 DEPLOY_DIR=/host/$SCRIPT_DIR/../qaas-web/deployment
@@ -61,7 +62,7 @@ DEPLOY_DIR=/host/$SCRIPT_DIR/../qaas-web/deployment
 # docker run --user root -p 81:80 --restart on-failure:1 --entrypoint /host/$SCRIPT_DIR/../qaas-web/deployment/entrypoint.sh --hostname $(hostname) ${mount_args[*]} ${env_args[*]} -v /:/host -v mysql_data:/var/lib/mysql -v /lib/modules:/lib/modules -v /tmp/tmp:/tmp/tmp -v /dev:/dev --pid=host --ipc=host -w /host/$(pwd) --security-opt seccomp=./qaas-docker-seccomp-profile.json -it local_image_qaas:latest 
 #docker run --user root -p 81:80 --restart unless-stopped --entrypoint /host/$SCRIPT_DIR/../qaas-web/deployment/entrypoint.sh --hostname $(hostname) ${mount_args[*]} ${env_args[*]} -v /:/host -v mysql_data:/var/lib/mysql -v /lib/modules:/lib/modules -v /tmp/tmp:/tmp/tmp -v /dev:/dev --pid=host --ipc=host -w /host/$(pwd) --security-opt seccomp=./qaas-docker-seccomp-profile.json -it local_image_qaas:latest 
 # use entrypoint.sh script to wrap various restart command.  The script will end with "su qaas" to start an interactive shell without quitting as qaas user.
-docker run --user root -p 80:80 -p 443:443 -p 3000:3000 --restart unless-stopped  --hostname $(hostname) ${mount_args[*]} ${env_args[*]} -v /:/host -v mysql_data:/var/lib/mysql -v letsencrypt_data:/etc/letsencrypt -v mods_enabled_data:/etc/apache2/mods-enabled -v htpasswd_data:/etc/apache2/auth -v /lib/modules:/lib/modules -v /tmp/tmp:/tmp/tmp -v /dev:/dev --pid=host --ipc=host -w /host/$(pwd) \
+docker run --user root -p 80:80 -p 443:443 -p 3000:3000 --restart unless-stopped  --hostname $(hostname) ${mount_args[*]} ${env_args[*]} -v /:/host -v mysql_data:/var/lib/mysql -v letsencrypt_data:/etc/letsencrypt -v mods_enabled_data:/etc/apache2/mods-enabled -v htpasswd_data:/etc/apache2/auth -v www_html_data:/var/www/html -v /lib/modules:/lib/modules -v /tmp/tmp:/tmp/tmp -v /dev:/dev --pid=host --ipc=host -w /host/$(pwd) \
   --security-opt seccomp=./qaas-docker-seccomp-profile.json -it local_image_qaas:latest ${DEPLOY_DIR}/entrypoint.sh
 
 #container_id=$(docker run --rm  ${mount_args[*]} ${env_args[*]} -v /:/host -v /usr/src/linux-headers-$(uname -r):/usr/src/linux-headers-$(uname -r) -v /lib/modules:/lib/modules -v /usr/src/linux-headers-4.4.0-62:/usr/src/linux-headers-4.4.0-62 -v /tmp/tmp:/tmp/tmp -v /dev:/dev -v /usr/include:/usr/include --pid=host --ipc=host -d -it --privileged local_image_qaas:latest )
