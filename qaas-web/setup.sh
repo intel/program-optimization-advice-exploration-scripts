8090015edf0bc7f2d6c285e53bda829347f2a6b3
yue.jiao <yuejiao@fxilab165.an.intel.com>
2024-01-17 19:53:22 -0600

    added htpassword file generation in qaas web setup.sh

--- qaas-web/setup.sh
+++ qaas-web/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 me=$(basename $(pwd))
+HTPASSWD_FILE="/etc/apache2/auth/.htpasswd"
+USERNAME="qaas"
 
 if [[ ${USER} != "qaas" ]]; then
   echo "OUTSIDE container setting up $me"
@@ -8,6 +10,8 @@ if [[ ${USER} != "qaas" ]]; then
 else
   echo -n "Enter Qaas DB user password:"
   read -s QAAS_DB_PASSWORD
+  echo "Setting up QaaS Web User Using Same Password As Database: " 
+  echo -n $QAAS_DB_PASSWORD | htpasswd -i -c $HTPASSWD_FILE $USERNAME
   echo "INSIDE container setting up $me"
   sed 's/#DB_USER_NAME#/qaas/g ; s/#DB_USER_PASSWD#/'${QAAS_DB_PASSWORD}'/g' ./apps/config/qaas-web.conf.template > ./apps/config/qaas-web.conf
   cd deployment
